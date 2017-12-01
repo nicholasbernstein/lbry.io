896618d9abaf240347aa8e9bfc2e7c5eb5a692dc
reillysmith <reilly@lbry.io>
2017-11-30 16:03:06 -0800

    Update and rename get.php to rubin.php

--- view/template/download/rubin.php
+++ view/template/download/rubin.php
@@ -5,7 +5,7 @@
 <?php echo View::render('nav/_header', ['isDark' => false]) ?>
 
 <main class="column-fluid">
-  <div class="span7">
+  <div class="span12">
     <div class="cover cover-dark cover-dark-grad content content-stretch content-dark">
       <h1><?php echo __('download.for-os', ['%os%' => $osTitle]) ?> <span class="<?php echo $osIcon ?>"></span></h1>
       <?php if ($downloadUrl): ?>
@@ -40,6 +40,10 @@
             <?php endif ?>
             Like source code? Go <a href="https://github.com/lbryio/lbry-app" class="link-primary">here</a>.<P><P>
             <img src="https://spee.ch/5a3e08d52dd2d7cb1c63a480b45dea8b4679cf01/lbryget-gif-mastertest.gif" />
+                <?php echo View::render('download/_list', [
+                  'excludeOs' => $os
+                ]) ?>
+                <?php echo View::render('download/_social') ?>
           </div>
         </div>
       <?php else: ?>
@@ -47,12 +51,6 @@
         <?php echo View::render('download/_signup') ?>
       <?php endif ?>
     </div>
-  </div>
-  <div class="span5">
-    <?php echo View::render('download/_list', [
-      'excludeOs' => $os
-    ]) ?>
-    <?php echo View::render('download/_social') ?>
   </div>
 </main>
 
