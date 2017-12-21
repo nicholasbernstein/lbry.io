b385a16510d4665dece18d3191146bd82f2efd44
blckchnprncss <32848677+blckchnprncss@users.noreply.github.com>
2017-12-21 11:40:01 -0500

    img update

--- view/template/page/college.php
+++ view/template/page/college.php
@@ -1,36 +1,42 @@
-<?php Response::setMetaDescription(__('description.learn')) ?>
-<?php Response::setMetaTitle(__('title.learn')) ?>
 <?php Response::addJsAsset('https://cdn.optimizely.com/js/9525271075.js') ?>
-<?php echo View::render('nav/_header', ['isDark' => true, 'isAbsolute' => true]) ?>
+<?php echo View::render('nav/_header', ['isDark' => false, 'isAbsolute' => true]) ?>
 <main>
-  <div class="cover cover-dark cover-center cover-full cover--dark-overlay" style="background-image:url(https://spee.ch/e/shirtless-chang.jpg)">
+  <div class="cover cover-dark cover-center cover-full" style="background-image:url(/img/dave-phil-lbryio.png)">
+  <div class="cover cover-dark cover-center cover-full" style="background-image:url(/img/college-page-img-1.jpg>)
     <div class="content content-dark text-center">
-      <h1 class="cover-title cover-title-tile cover-item--outline">
-        LBRY Goes To College
+      <h1 class="cover-title cover-title-tile cover-title-flat">
+        The Rubin Report<br/>
+        YouTube Week
+        The Future Belongs<br/>
+        To Those Who Build It
       </h1>
-      <h3 class="cover-item--outline">
-        Earn Credits More Valuable<br/>
-        Than Those In The Classroom
-      </h3>
+      <h3>
+          Brought to you by LBRY
+          We Could Use A Little Help
+        </h3>
       <div class="spacer1">
-        <a href="#chang" class="btn-primary btn-large">Put A Shirt On</a>
+        <a href="/get" class="btn-primary btn-large">Watch Rubin on LBRY</a>
+        <a href="/get" class="btn-primary btn-large">Build With Us</a>
+      </div>
+      <div class="meta">
+        Desktop only. Mobile coming soon.
+        Help Out. Earn Crypto. It's Way Better Than An MLM.
       </div>
     </div>
   </div>
-  <div class="cover cover-light" id="chang">
+  <div class="cover cover-light">
     <div class="content content-light content-readable">
-      <h3>What's going on here? Where's Chang's Shirt?</h3>
-      <p>I burned it off and put it on a blockchain.</p>
+      <h3>What's going on here?</h3>
+      <p>LBRY is a departure from the platforms of yesterday.</p>
+      <p>Watch The Rubin Report YouTube Week on the only community-run video app without ads or middlemen.</p>
+      <p>We're a bunch of nerds working on a cool project that we think college students might really like.</p>
+      <p>If the students we talk to more than just like it, we want to support them in spreading the word around their campus.</p>
       <h3>Okay. But why?</h3>
-      <p>Enter your email below and we'll tell you more.</p>
-      <?php echo View::render('mail/_subscribeForm', [
-        'tag' => 'college',
-        'submitLabel' => 'Enroll',
-        'hideDisclaimer' => true,
-        'largeInput' => true,
-        'btnClass' => 'btn-alt btn-large',
-      ]) ?>
+      <p>Open-source and decentralized, LBRY is shaped entirely by the creators and community who use it. <b>Free speech and censorship-resistance are baked into the design.</b></p>
+        <p>There are lots of nifty aspects to how LBRY works (pronounced, "library"). Learn from the old-timers on <a href="https://chat.lbry.io"><u>our Discord</u></a>.</p>
+      <div class="text-center">
+        <a href="/get" class="btn-alt btn-large">Download the App</a>
+        <a href="/get" class="btn-alt btn-large">I'm 'Bout That Life'</a>
+      </div>
     </div>
   </div>
-</main>
-<?php echo View::render('nav/_footer') ?>
